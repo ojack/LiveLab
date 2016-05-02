3d57ef1b0d1a77798307061636223833b04871e0
JesseRicke <thefertile@gmail.com>
2016-05-01 21:23:12 -0400

    test commit

--- app/main.js
+++ app/main.js
@@ -6,8 +6,15 @@ var BASE_SOCKET_URL = "wss://localhost";
 var BASE_SOCKET_PORT = 8000;
 
  var webrtc, chatlog, osc;
+// had to change start function to control device enumeration
+ function start() {
 
- window.onload = function(){
+    if (window.stream) {
+        window.stream.getTracks().forEach(function(track) {
+            track.stop();
+        });
+    }
+
      // grab the room from the URL
     var room = location.search && location.search.split('?')[1];
     chatlog = document.getElementById("chatlog");
@@ -23,7 +30,7 @@ var BASE_SOCKET_PORT = 8000;
         debug: false,
         detectSpeakingEvents: true,
         autoAdjustMic: false
-     });
+     })
 
     //create div element for local osc streams
     var streamDiv = document.createElement('div');
@@ -59,21 +66,38 @@ var BASE_SOCKET_PORT = 8000;
             var d = document.createElement('div');
             d.className = 'videoContainer';
             d.id = 'container_' + webrtc.getDomId(peer);
-            d.appendChild(video);
+
             var vol = document.createElement('div');
             vol.id = 'volume_' + peer.id;
             vol.className = 'volume_bar';
+            d.appendChild(vol);
+
+            d.appendChild(video);
             video.onclick = function () {
                 video.style.width = video.videoWidth + 'px';
                 video.style.height = video.videoHeight + 'px';
             };
-            d.appendChild(vol);
+
             var streamDiv = document.createElement('div');
             streamDiv.className = "stream-holder";
             osc.addPeer(peer.id, streamDiv);
             d.appendChild(streamDiv);
+            
+            // audio output section
+            var audioOut = document.createElement('div');
+            audioOut.className = 'outputSelector';
+            audioOut.id = 'audioOut_' + peer.id;
+            var audioOutLabel = document.createElement('label');
+            audioOutLabel.innerHTML = 'Select peer audio output: ';
+            audioOut.appendChild(audioOutLabel);
+            var audioOutSelector = document.createElement('select');
+            audioOut.appendChild(audioOutSelector);
+            d.appendChild(audioOut);
+
             remotes.appendChild(d);
-        }
+        };
+        // to enumerate audio outs for new peer
+        gotDevices();
     });
 
     webrtc.on('videoRemoved', function (video, peer) {
@@ -129,7 +153,6 @@ function showVolume(el, volume) {
     }
 }
 
-
 function setRoom(name) {
     $('#createRoom').remove();
     $('h1').text(name);
@@ -137,5 +160,76 @@ function setRoom(name) {
     $('body').addClass('active');
 }
 
+// FIX: getOuts sould happen after start() and on.videoAdded
+document.getElementById('getOuts').onclick = function() {getOuts()};
+function getOuts(){
+    navigator.mediaDevices.enumerateDevices()
+    .then(gotDevices)
+    .catch(errorCallback);
+}
 
-          
+// audio out section
+function gotDevices(deviceInfos) {
+  console.log('window stream' + window.stream);
+  var masterOutputSelector = document.createElement('select');
+
+  for (var i = 0; i !== deviceInfos.length; ++i) {
+    var deviceInfo = deviceInfos[i];
+    var option = document.createElement('option');
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === 'audiooutput') {
+      console.info('Found audio output device: ', deviceInfo.label);
+      option.text = deviceInfo.label || 'speaker ' +
+          (masterOutputSelector.length + 1);
+      masterOutputSelector.appendChild(option);
+    } else {
+      console.log('Found non audio output device: ', deviceInfo.label);
+    }
+  }
+
+  // Clone the master outputSelector and replace outputSelector placeholders.
+  var allOutputSelectors = document.querySelectorAll('select');
+  for (var selector = 0; selector < allOutputSelectors.length; selector++) {
+    var newOutputSelector = masterOutputSelector.cloneNode(true);
+    newOutputSelector.addEventListener('change', changeAudioDestination);
+    allOutputSelectors[selector].parentNode.replaceChild(newOutputSelector,
+        allOutputSelectors[selector]);
+  }
+}
+
+// Attach audio output device to the provided media element using the deviceId.
+function attachSinkId(element, sinkId, outputSelector) {
+
+  if (typeof element.sinkId !== 'undefined') {
+    element.setSinkId(sinkId)
+    .then(function() {
+      console.log('Success, audio output device attached: ' + sinkId + ' to ' +
+          'element with ' + element.id + ' as source.');
+    })
+    .catch(function(error) {
+      var errorMessage = error;
+      if (error.name === 'SecurityError') {
+        errorMessage = 'You need to use HTTPS for selecting audio output ' +
+            'device: ' + error;
+      }
+      console.error(errorMessage);
+      // Jump back to first output device in the list as it's the default.
+      outputSelector.selectedIndex = 0;
+    });
+  } else {
+    console.warn('Browser does not support output device selection.');
+  }
+}
+
+function changeAudioDestination(event) {
+  var deviceId = event.target.value;
+  var outputSelector = event.target;
+  var element = event.path[2].childNodes[1];
+  attachSinkId(element, deviceId, outputSelector);
+}        
+
+function errorCallback(error) {
+  console.log('Error: ', error);
+}
+
+start();
