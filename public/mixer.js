2e94853ec5d3400d31182570c19231d4af607089
Alexander Cobleigh <angellus.falsus@gmail.com>
2016-06-11 02:20:27 +0200

    started work on fixing issue #46

--- public/mixer.js
+++ public/mixer.js
@@ -1,6 +1,6 @@
 // TO DO: add events for adding and removing peers, forwarded from mixer window
 
-console.log("opened page");
+console.log("mixer.js: opened page");
 var seriously, sources, canvas, blend, effects, streams, resize;
 var isFullScreen = false;
 var previousState = {};
@@ -8,7 +8,7 @@ var RESIZE_WIDTH = 800;
 var RESIZE_HEIGHT = 600;
 
 document.addEventListener('sourcesAdded', function(e){
-   console.log("init vid");
+    console.log("init vid");
     canvas = document.createElement('canvas');
     canvas.id = "mixerCanvas";
     canvas.width = window.innerWidth;
@@ -27,20 +27,6 @@ document.addEventListener('sourcesAdded', function(e){
            toggleFullscreen();
        }
     }
-  /* window.onresize = function(event){
-   // canvas.width = window.innerWidth;
-    //canvas.height = window.innerHeight;
-
-    console.log(window.innerWidth);
-  /*  resize.width = window.innerWidth;
-    resize.height = window.innerHeight;*/
-  
-   /* for(var i = 0; i < sources.length; i++){
-      
-      sources[i].reformat.width = canvas.width;
-      sources[i].reformat.height = canvas.height;
-    }
-   }*/
 });
 
 document.addEventListener('osc', function(e){
@@ -75,107 +61,86 @@ document.addEventListener('contrast', function(e){
 });
 
 document.addEventListener('source', function(e){
-  console.log(e.detail);
-   console.log(streams);
-   sources[e.detail.source].div.src = streams[e.detail.stream].src;
-
-  // this.mixerState.sources[i].outputDiv.src = this.streams[e.target.value].src;*/
+    console.log(e.detail);
+    console.log(streams);
+    sources[e.detail.source].div.src = streams[e.detail.stream].src;
 });
 
 document.addEventListener('updateState', function(e){
-    //alert("MIXER EVENT");
     console.log(e.detail);
     updateEffectsFromState(e.detail);
-   // blend.mode = e.detail;
-
-   // blend.update();
 });
 
 function initSeriously(initialState){
     seriously = new Seriously();
-     target = seriously.target('#mixerCanvas');
-   initEffectsFromState(initialState);
+    target = seriously.target('#mixerCanvas');
+    initEffectsFromState(initialState);
 }
 
 function initEffectsFromState(state){
- 
-   console.log("init seriously");
-      sources = [];
-      effects = [];
-      streams = state.streams;
-      console.log(state);
-      for(var i = 0; i < state.sources.length; i++){
+    console.log("init seriously");
+    sources = [];
+    effects = [];
+    streams = state.streams;
+    console.log(state);
+    for(var i = 0; i < state.sources.length; i++){
         var source = seriously.source("#"+state.sources[i].outputDiv.id);
         if(state.sources[i].outputDiv.src != state.sources[i].src){
-          state.sources[i].outputDiv.src = state.sources[i].src;
+            state.sources[i].outputDiv.src = state.sources[i].src;
         }
         var reformat = seriously.transform('reformat');
         reformat.mode = 'cover';
         reformat.width = canvas.width;
         reformat.height = canvas.height;
         reformat.source = source;
-         var contrast = seriously.effect('brightness-contrast');
+        var contrast = seriously.effect('brightness-contrast');
         if(i == 1 || i == 2){
-          contrast.contrast = 3.0;
-          contrast.brightness = 3.0;
+            contrast.contrast = 3.0;
+            contrast.brightness = 3.0;
         } else {
-          contrast.contrast = 1.0;
-          contrast.brightness = 1.0;
+            contrast.contrast = 1.0;
+            contrast.brightness = 1.0;
         }
         contrast.source = reformat;
         var obj = {src: source, contrast: contrast, reformat: reformat, div: state.sources[i].outputDiv};
         sources.push(obj);
-      }
+    }
     //  var blend;
     console.log(sources);
     console.log(state.effects);
-      for(var i = 0; i < state.effects.length; i++){
+    for(var i = 0; i < state.effects.length; i++){
         effect = seriously.effect(state.effects[i].type);
         for(prop in state.effects[i]){
             if(prop == "bottom" || prop == "top"){
-             //  effect[bottom] = sources[0].reformat;
-            
-             var hey = state.effects[i][prop];
-              console.log(hey);
+                //  effect[bottom] = sources[0].reformat;
 
-             if(hey=="blend"){
-              
-              effect[prop] = effects[0];
-             } else {
-                
-              effect[prop] = sources[hey].contrast;
-             }
-            
+                var hey = state.effects[i][prop];
+                console.log(hey);
+
+                if(hey=="blend"){
+                    effect[prop] = effects[0];
+                } else {
+                    effect[prop] = sources[hey].contrast;
+                }
+
 
             } else {
                 effect[prop] = state.effects[i][prop];
             }
         }
-       
         console.log(effect);
         effects.push(effect);
-      
-      }
-      
-  
-    // now do the same for the target canvas
-   
-
+    }
     // connect any node as the source of the target. we only have one.
-    /* resize = seriously.transform('reformat');
-        resize.mode = 'cover';
-        resize.width = canvas.width;
-        resize.height = canvas.height;
-        resize.source = source;*/
     if(effects.length > 0){
         console.log("adding source", blend);
         target.source = effects[effects.length-1];
-       //target.source = effects[state.effects.length-1];
+        //target.source = effects[state.effects.length-1];
     } else {
         target.source = sources[0];
     }
-   // target.source = resize;
-seriously.go();
+    // target.source = resize;
+    seriously.go();
 }
 
 function updateEffectsFromState(state){
@@ -183,24 +148,24 @@ function updateEffectsFromState(state){
 }
 
 function toggleFullscreen(){
-  var isFirefox = typeof InstallTrigger !== 'undefined';
-     var isChrome = !!window.chrome && !!window.chrome.webstore;
-        canvas.focus();
-        if (!isFullScreen) {
-            if (isFirefox == true) {
-                canvas.mozRequestFullScreen();
-            }
-            if (isChrome == true) {
-                canvas.webkitRequestFullScreen();
-            }
-            isFullScreen = true;
-        } else {
-            if (isFirefox == true) {
-                canvas.mozCancelFullscreen();
-            }
-            if (isChrome == true) {
-                canvas.webkitExitFullscreen();
-            }
-            isFullScreen = false;
-        }   
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    var isChrome = !!window.chrome && !!window.chrome.webstore;
+    canvas.focus();
+    if (!isFullScreen) {
+        if (isFirefox == true) {
+            canvas.mozRequestFullScreen();
+        }
+        if (isChrome == true) {
+            canvas.webkitRequestFullScreen();
+        }
+        isFullScreen = true;
+    } else {
+        if (isFirefox == true) {
+            canvas.mozCancelFullscreen();
+        }
+        if (isChrome == true) {
+            canvas.webkitExitFullscreen();
+        }
+        isFullScreen = false;
+    }   
 }
