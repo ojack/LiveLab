d122f6b1191bb220d18b711bfed4fe74611e6455
Olivia Jack <ojackson@gmail.com>
2018-01-24 12:29:42 -0500

    updated media list style

--- public/bundle.js
+++ public/bundle.js
@@ -1790,13 +1790,13 @@ function mediaListView (state, emit) {
   return html`
 
     <div class="pa3 dib">
-        <table cellspacing="0" cellpadding="0" class="h5 collapse overflow-scroll">
+        <table cellspacing="0" cellpadding="0" class="collapse overflow-scroll">
           <thead>
             <tr>
+              <th>PEER</th>
               <th>NAME</th>
               <th>ID</th>
               <th>KIND</th>
-              <th>PEER</th>
             </tr>
           </thead>
           <tbody>
@@ -1805,11 +1805,11 @@ function mediaListView (state, emit) {
             var className = id == state.ui.inspector.trackId ? "bg-gray pointer" : "dim pointer"
             console.log(id, state.ui.inspector.trackId)
             return html`
-              <tr class=${className} onclick=${()=>{emit('user:setInspectMedia', id)}}>
-                <td class="pa1">${media.name}</td>
-                <td class="pa1">${media.track.id}</td>
-                <td class="pa1">${media.track.kind}</td>
-                <td class="pa1">${state.peers.byId[media.peerId].nickname}</td>
+              <tr class=${className} style="height:20px" onclick=${()=>{emit('user:setInspectMedia', id)}}>
+                <td class="pa1" style="height:20px">${state.peers.byId[media.peerId].nickname}</td>
+                <td class="pa1" style="height:20px">${media.track.kind}</td>
+                <td class="pa1" style="height:20px">${media.name}</td>
+                <td class="pa1" style="height:20px">${media.track.id}</td>
               </tr>
             `
           })}
