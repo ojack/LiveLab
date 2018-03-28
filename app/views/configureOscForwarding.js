8521499f77baad1899be76314d747e06b20330c7
Olivia Jack <ojackson@gmail.com>
2018-03-27 19:51:59 -0500

    removed error for forwarding

--- app/views/configureOscForwarding.js
+++ app/views/configureOscForwarding.js
@@ -7,27 +7,20 @@ const input = require('./components/input.js')
 module.exports = configureOscForwarding
 
 function configureOscForwarding (oscInfo, emit, showElement) {
-
-
   return html`
-
     ${Modal({
       show: showElement,
-      header: "Configure OSC Forwarding",
+      header: 'Configure OSC Forwarding',
       contents: html`<div class="pa3 f6 fw3">
-
             ${input('port', 'port', {
               value: oscInfo.port,
               onkeyup: (e) => {
                 emit('ui:setLocalOscForward', e.target.value)
               }
             })}
-
             <div class="f6 link dim ph3 pv2 mb2 dib white bg-dark-pink pointer" onclick=${() => (emit('user:setLocalOscForward', oscInfo))}>Start</div>
-            <p class="red">error would be here</p>
         </div>`,
       close: () => (emit('ui:doneConfiguringOsc'))
     })}
     `
-
 }
--- public/bundle.js
+++ public/bundle.js
@@ -2679,29 +2679,22 @@ const input = require('./components/input.js')
 module.exports = configureOscForwarding
 
 function configureOscForwarding (oscInfo, emit, showElement) {
-
-
   return html`
-
     ${Modal({
       show: showElement,
-      header: "Configure OSC Forwarding",
+      header: 'Configure OSC Forwarding',
       contents: html`<div class="pa3 f6 fw3">
-
             ${input('port', 'port', {
               value: oscInfo.port,
               onkeyup: (e) => {
                 emit('ui:setLocalOscForward', e.target.value)
               }
             })}
-
             <div class="f6 link dim ph3 pv2 mb2 dib white bg-dark-pink pointer" onclick=${() => (emit('user:setLocalOscForward', oscInfo))}>Start</div>
-            <p class="red">error would be here</p>
         </div>`,
       close: () => (emit('ui:doneConfiguringOsc'))
     })}
     `
-
 }
 
 },{"./components/input.js":18,"./components/modal.js":19,"choo/html":47}],28:[function(require,module,exports){
