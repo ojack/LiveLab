f611e7557eae824f817a35d4fc9342f220644a5e
Damián Silvani <munshkr@gmail.com>
2021-03-17 12:11:32 -0300

    Use MultiPeer channel for syncing between browserglue instances

--- app/views/browserglue.js
+++ app/views/browserglue.js
@@ -6,6 +6,12 @@ const { button } = require('./formElements.js')
 module.exports = class Browserglue extends Component {
   constructor (id, state, emit) {
     super(id)
+
+    // Multipeer channel for keeping browserglue channels in sync between peers
+    this.syncChannel = state.multiPeer.addChannel('browserglue-sync', {});
+    this.user = state.user;
+    this.channels = {};
+
     const bg = new browserglue.Client()
     this.isConnected = false
     console.log('created browserglue', bg)
@@ -20,24 +26,35 @@ module.exports = class Browserglue extends Component {
       this.rerender()
     });
     bg.on('change', channels => console.log("[change]", channels));
-    bg.on('add-channel', ({ path }) => console.log("[add-channel]", path));
-    bg.on('remove-channel', ({ path }) => console.log("[remove-channel]", path));
+    bg.on('add-channel', ({ path }) => {
+      console.log("[add-channel]", path);
+      this.syncChannel.send('message', { event: 'add-channel', path });
+    });
+    bg.on('remove-channel', ({ path }) => {
+      console.log("[remove-channel]", path);
+      this.syncChannel.send('message', { event: 'remove-channel', path });
+    });
     bg.on('bind-port', ({ path, port }) => console.log("[bind-port]", path, port));
     bg.on('subscribe-port', ({ path, port }) => console.log("[subscribe-port]", msg));
     bg.on('unsubscribe-port', ({ path, port }) => console.log("[unsubscribe-port]", msg));
     this.bg = bg
+
+    this._handleSyncMessages();
   }
 
   update () {
     return false
   }
 
-  async addChannel({name = 'test', port = '54321'}) {
-    const thisChannel = await this.bg.addChannel(name, port)
+  async addChannel({ path = '/default', port = '54321' }) {
+    const thisChannel = await this.bg.addChannel(path, port)
 
     thisChannel.on('message', async blob => {
+      // Forward messages to all peers through the sync channel
+      this.syncChannel.send('message', { event: 'message', path, data: blob });
+
       const text = await blob.text();
-      console.log("[/bar]", text);
+      console.log(path, text);
     });
   }
 
@@ -52,5 +69,36 @@ module.exports = class Browserglue extends Component {
         </div>`
       : `browserglue not connected`}
     </div>`
+  }
+
+  _handleSyncMessages() {
+    this.syncChannel.on('message', (message, peer) => {
+      // Ignore messages sent by the same peer
+      if (peer === this.user) return;
+
+      // Handle sync messages
+      switch (message.event) {
+        case 'message': {
+          const { path, data } = message;
+          const channel = this.channels[path];
+          if (channel) channel.publish(data);
+          break;
+        }
+        case 'add-channel': {
+          const { path } = message;
+          this.bg.addChannel(path);
+          this.channels[path] = { owner: peer };
+          break;
+        }
+        case 'remove-channel': {
+          const { path } = message;
+          this.bg.removeChannel(path);
+          delete this.channels[path];
+          break;
+        }
+        default:
+          console.warn('[browserglue] Unhandled sync message:', message);
+      }
+    });
   }
 }
